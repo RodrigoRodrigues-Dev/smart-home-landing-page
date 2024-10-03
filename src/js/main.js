c3f9c240951c37ee69a7606352589b5d0d3cad9f
Rodrigo Roigues <rs9844660@gmail.com>
2024-10-02 22:29:42 -0300

    feat: Implement full header component with mobile navigation, including HTML structure, JavaScript toggle functionality, SCSS styling, responsive design, and improved user experience.

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btnOpenHeader = document.querySelector('.header-mobile__nav-toggle');
+    const btnCloseHeader = document.querySelector('.header-mobile__menu-close');
+    const pseudoElement = document.querySelector('.header-mobile__menu--after');
+    const menuList = document.querySelectorAll('.header-mobile__menu-item');
+
+    const buttons = [btnOpenHeader, btnCloseHeader, pseudoElement];
+
+    if (btnOpenHeader && btnCloseHeader && pseudoElement) {
+        buttons.forEach(button => button.addEventListener('click', toggleMobileMenu));
+    }
+
+    menuList.forEach(item => item.addEventListener('click', toggleMobileMenu));
+});
+
+const toggleMobileMenu = () => {
+    const headerMobile = document.querySelector('.header-mobile__menu');
+
+    if (headerMobile) {
+        headerMobile.classList.toggle('header-mobile__menu--active');
+        
+        if (headerMobile.classList.contains('header-mobile__menu--active')) {
+            disableScroll();
+        } else {
+            enableScroll();
+        }
+    }
+};
+
+const disableScroll = () => {
+    document.body.style.overflow = 'hidden';
+};
+
+const enableScroll = () => {
+    document.body.style.overflow = '';
+};
