4e22ed56cf8164d9d4bd78ecf235e29530d9412a
Rodrigo Roigues <rs9844660@gmail.com>
2024-10-15 17:14:58 -0300

    feat: Add testimonials section with client feedback and profile details

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btnOpenHeader = document.querySelector('.header-mobile__toggle');
-    const btnCloseHeader = document.querySelector('.header-mobile__close');
-    const pseudoElement = document.querySelector('.header-mobile__menu--after');
-    const menuList = document.querySelectorAll('.header-mobile__item');
+const btnOpenHeader = document.querySelector('.header-mobile__toggle');
+const btnCloseHeader = document.querySelector('.header-mobile__close');
+const pseudoElement = document.querySelector('.header-mobile__menu--after');
+const menuList = document.querySelectorAll('.header-mobile__item');
+const buttons = [btnOpenHeader, btnCloseHeader, pseudoElement];
 
-    const buttons = [btnOpenHeader, btnCloseHeader, pseudoElement];
-
-    if (btnOpenHeader && btnCloseHeader && pseudoElement) {
-        buttons.forEach(button => button.addEventListener('click', toggleMobileMenu));
-    }
-
-    menuList.forEach(item => item.addEventListener('click', toggleMobileMenu));
-});
-
+// Funções de manipulação do menu mobile
 const toggleMobileMenu = () => {
     const headerMobile = document.querySelector('.header-mobile__menu');
 
     if (headerMobile) {
         headerMobile.classList.toggle('header-mobile__menu--active');
-        
+
         if (headerMobile.classList.contains('header-mobile__menu--active')) {
             disableScroll();
         } else {
@@ -34,3 +26,27 @@ const disableScroll = () => {
 const enableScroll = () => {
     document.body.style.overflow = '';
 };
+
+if (btnOpenHeader && btnCloseHeader && pseudoElement) {
+    buttons.forEach(button => button.addEventListener('click', toggleMobileMenu));
+}
+
+menuList.forEach(item => item.addEventListener('click', toggleMobileMenu));
+
+// Controle dos depoimentos/testemunhos
+const testimonials = document.querySelectorAll('.testimonials__content');
+const btnNext = document.querySelector('.testimonials__next');
+const btnPrev = document.querySelector('.testimonials__prev');
+let currentItem = 0;
+
+btnNext.addEventListener('click', () => {
+    testimonials[currentItem].classList.remove('testimonials__content--visible');
+    currentItem = (currentItem + 1) % testimonials.length;
+    testimonials[currentItem].classList.add('testimonials__content--visible');
+});
+
+btnPrev.addEventListener('click', () => {
+    testimonials[currentItem].classList.remove('testimonials__content--visible');
+    currentItem = (currentItem - 1 + testimonials.length) % testimonials.length;
+    testimonials[currentItem].classList.add('testimonials__content--visible');
+});
